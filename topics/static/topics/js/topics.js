a7fd64a3768e58ac2b15a3876d3183137b6ee0da
cpython666 <2942581284@qq.com>
2024-07-15 19:57:41 +0800

    docs(文档修改)：更新readme，增加了注意事项。不得用于非法，作者概不负责。

--- topics/static/topics/js/topics.js
+++ topics/static/topics/js/topics.js
@@ -3,22 +3,13 @@ const topics = [
     id: 1,
     title: '何为动态网页',
     des:'何为动态网页！原来这就是动态网页啊！',
-    content: 'What is 2+2?',
+    content: '获取显示的数据，字段包括，标题，发布时间，作者，页面链接',
     category: 'Math',
-    difficulty: 'Easy',
-    points: ['', '3', '4', '5'],
-    correct_answer: '4',
-    		question:"就是ajax请求而已，不过如此~",
-		answer:'直接使用requests请求即可',
+    difficulty: 初级，中级，高级，终极,
+    points: [xpath, 开发者工具,网络面板],//考点
+    // correct_answer: '4',
+    question:"就是ajax请求而已，不过如此~",
+    answer:'直接使用requests请求即可',
   },
-  {
-    id: 2,
-    title: 'Sample Quiz 2',
-    content: 'What is the capital of France?',
-    category: 'Geography',
-    difficulty: 'Easy',
-    options: ['Paris', 'London', 'Berlin', 'Madrid'],
-    correct_answer: 'Paris'
-  },
-  // 更多题目
+
 ];
