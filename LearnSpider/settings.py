a6c662e97907892917a6e725fa6a36b330e68f7b
cpython666 <2942581284@qq.com>
2025-01-20 21:46:33 +0800

    feat：3.0项目再升级，不再使用sqlite作为数据库，使用mysql，稍微正规化项目，为线上部署做准备

--- LearnSpider/settings.py
+++ LearnSpider/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -67,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "LearnSpider.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -77,7 +75,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -96,7 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -134,3 +130,14 @@ ALLOWED_HOSTS = [
 
 # PORT=8001
 topics_path_prefix = "topic/"
+
+# settings.py 的末尾添加以下代码
+
+try:
+    from .local_settings import *
+    from .secret_settings import *
+except ImportError as e:
+    print(e)
+    pass
+
+print(DEBUG)
