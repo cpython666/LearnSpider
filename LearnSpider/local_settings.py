e2911fc2f624ca3e38b127d8fd1c2cd79f3b9cae
cpython666 <2942581284@qq.com>
2025-01-20 21:42:09 +0800

    feat：3.0项目再升级，不再使用sqlite作为数据库，使用mysql，稍微正规化项目，为线上部署做准备

---
+++ LearnSpider/local_settings.py
@@ -0,0 +1,38 @@
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "dbname",
+#         "HOST": "127.0.0.1",
+#         "PORT": 3306,
+#         "USER": "root",
+#         "PASSWORD": "1234",
+#     }
+# }
+SECRET_KEY = "your-secret-key"
+# settings
+# 多语言配置更改语言为中文
+LANGUAGE_CODE = "zh-hans"
+# 时区
+TIME_ZONE = "Asia/Shanghai"
+USE_TZ = True
+APPEND_SLASH = True
+
+SITE_URL = "http://learnspider.vip"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:63342",  # 允许的前端地址
+    "http://localhost:8005",  # 允许的前端地址（如果有必要）
+    "http://localhost:5173",  # 允许的前端地址（如果有必要）
+    "http://localhost:4173",  # 允许的前端地址（如果有必要）
+    "http://localhost:4000",  # 允许的前端地址（如果有必要）
+    "http://www.learnspider.vip",
+    "http://learnspider.vip",
+    "http://localhost",
+    "http://127.0.0.1",
+]
+ALLOWED_HOSTS = [
+    "www.learnspider.vip",
+    "learnspider.vip",
+    "127.0.0.1",
+    "localhost",
+]
--- topics/models.py
+++ topics/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import json
+
+
 class Topics(models.Model):
     DIFFICULTY_CHOICES = [
         ('beginner', '初级'),
@@ -9,25 +11,31 @@ class Topics(models.Model):
     ]
 
     id = models.AutoField(primary_key=True)
-    order_id = models.PositiveIntegerField(blank=True, null=True,default=3,help_text='题目排序，根据难度分排序，会变')
-    title = models.CharField(blank=True,default='111',max_length=200,help_text='题目的标题，最好有趣个性化一点')
-    des = models.TextField(blank=True, null=True,default='暂无表述',help_text='题目的描述：简单创造一个背景故事')
-    goal = models.TextField(blank=True, null=True,default='暂无描述',help_text='题目的目标：掌握xxx')
-    question = models.TextField(blank=True, null=True,default='暂无题目要求',help_text='题目要求')
-    answer = models.CharField(blank=True,default='666',max_length=255,help_text='题目的答案')
-    category = models.CharField(blank=True, null=True,default='成神之路',max_length=100,help_text='题目类别：成神之路，xpath特训')
-    difficulty = models.CharField(blank=True, null=True,default='简单',max_length=12, choices=DIFFICULTY_CHOICES,help_text='难度')
-    difficulty_score = models.BigIntegerField(blank=True, null=True,default=200,help_text='难度分数，后续根据此字段排序order_id')
-    points = models.TextField(blank=True, null=True,default='暂未更新考点',help_text='本题的考点',)
-    published = models.BooleanField(blank=True, null=True,default=False,help_text='是否发布')
-    pass_status = models.BooleanField(blank=True, null=True,default=False,help_text='是否通过')
-    solution_txt = models.URLField(blank=True, null=True,default='暂无表述',help_text='题解，图文讲解')
-    solution_video = models.URLField(blank=True, null=True,default='暂无表述',help_text='视频讲解')
-    api_type = models.CharField(blank=True, null=True,max_length=255,default='直接对应视图',help_text='此题目的接口类型：直接对应视图，访问一个接口判断后决定是否返回视图，返回一个视图+【多个】api')
-    api_prefix = models.CharField(blank=True, null=True,max_length=255,default='topic/',help_text='topic/,view/,api/')
+    order_id = models.PositiveIntegerField(blank=True, null=True, default=3, help_text='题目排序，根据难度分排序，会变')
+    title = models.CharField(blank=True, default='111', max_length=200, help_text='题目的标题，最好有趣个性化一点')
+    des = models.TextField(blank=True, null=True, default='暂无表述', help_text='题目的描述：简单创造一个背景故事')
+    goal = models.TextField(blank=True, null=True, default='暂无描述', help_text='题目的目标：掌握xxx')
+    question = models.TextField(blank=True, null=True, default='暂无题目要求', help_text='题目要求')
+    answer = models.CharField(blank=True, default='666', max_length=255, help_text='题目的答案')
+    category = models.CharField(blank=True, null=True, default='成神之路', max_length=100,
+                                help_text='题目类别：成神之路，xpath特训')
+    difficulty = models.CharField(blank=True, null=True, default='简单', max_length=12, choices=DIFFICULTY_CHOICES,
+                                  help_text='难度')
+    difficulty_score = models.BigIntegerField(blank=True, null=True, default=200,
+                                              help_text='难度分数，后续根据此字段排序order_id')
+    points = models.TextField(blank=True, null=True, default='暂未更新考点', help_text='本题的考点', )
+    published = models.BooleanField(blank=True, null=True, default=False, help_text='是否发布')
+    pass_status = models.BooleanField(blank=True, null=True, default=False, help_text='是否通过')
+    solution_txt = models.URLField(blank=True, null=True, default='暂无表述', help_text='题解，图文讲解')
+    solution_video = models.URLField(blank=True, null=True, default='暂无表述', help_text='视频讲解')
+    api_type = models.CharField(blank=True, null=True, max_length=255, default='直接对应视图',
+                                help_text='此题目的接口类型：直接对应视图，访问一个接口判断后决定是否返回视图，返回一个视图+【多个】api')
+    api_prefix = models.CharField(blank=True, null=True, max_length=255, default='topic/',
+                                  help_text='topic/,view/,api/')
     response_path = models.TextField(blank=True, null=True, help_text='题目路径【文件名】')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
     class Meta:
         ordering = ['order_id']  # 默认按 order_id 排序
 
