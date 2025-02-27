f94f6788d8a1c89e4fb3e7f49baf0d55081637cf
cpython666 <2942581284@qq.com>
2025-02-28 00:17:24 +0800

    feat:(更新 完善备注)

--- topics/management/commands/fetch_news_data.py
+++ topics/management/commands/fetch_news_data.py
@@ -4,7 +4,7 @@ from topics.tasks import fetch_and_save_news
 
 # python manage.py fetch_news_data
 class Command(BaseCommand):
-    help = "批量更新题目的 category，规范化 category"
+    help = "采集一批热点新闻"
 
     def handle(self, *args, **kwargs):
         fetch_and_save_news()
