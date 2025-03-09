eda3fe7da2530f833ed2cfb5b82bf2202ec56e68
cpython666 <2942581284@qq.com>
2025-03-10 00:47:25 +0800

    fixed(只留下一百条新闻日志)

--- topics/tasks.py
+++ topics/tasks.py
@@ -22,7 +22,7 @@ def fetch_and_save_news():
             print(f'记录数量:{total}')
             if total > 100:
                 # 获取最新的 50 条记录的 ID
-                preserve_ids = list(NewsRequestHistory.objects.all().order_by("-id")[:50].values_list("id", flat=True))
+                preserve_ids = list(NewsRequestHistory.objects.all().order_by("-id")[:100].values_list("id", flat=True))
                 # 删除不在这些 ID 中的记录
                 NewsRequestHistory.objects.exclude(id__in=preserve_ids).delete()
             print(f"{platform.name} {platform.slug} 保存完成")
